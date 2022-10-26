fcd55cd30ad23eeb11a2961f75f326693e29decf
Doron Feldman <doronfeldman07@gmail.com>
2022-10-26 21:52:19 +0300

    use bcryptjs instead of bcrypt

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM node:16.17.0-alpine as base
 
-RUN apk add --update python3 make g++ && rm -rf /var/cache/apk/*
 RUN mkdir /opt/app && chown node:node /opt/app
 WORKDIR /opt/app
 
--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 
 export const hashPassword = async (password: string): Promise<string> => {
   return bcrypt.hash(password, 10)
