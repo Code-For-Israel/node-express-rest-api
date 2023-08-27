3d3617850f821e0864f079cdb54a2bd13c742adc
Botruzo <65567272+Botruzo@users.noreply.github.com>
2023-08-27 11:01:43 +0300

    add error message for location rejection

--- frontend/src/components/map/MapLocationDialog.tsx
+++ frontend/src/components/map/MapLocationDialog.tsx
@@ -2,6 +2,7 @@ import useStaticTranslation from '@/hooks/useStaticTranslation'
 import { Button, Dialog, DialogTitle, IconButton, Stack, Typography } from '@mui/material'
 import Image from 'next/image'
 import CloseIcon from 'public/icons/close.svg'
+import { useState } from 'react'
 
 type Props = {
   open: boolean
@@ -10,11 +11,19 @@ type Props = {
 }
 
 const MapLocationDialog = ({ open: openDialog, onClose, onLocationApproved }: Props) => {
+  const [error, setError] = useState<string | null>(null)
   const { t } = useStaticTranslation()
 
   const handleLocationSelected = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(onLocationApproved, onClose)
+      navigator.geolocation.getCurrentPosition(onLocationApproved, error => {
+        if (error.code == error.PERMISSION_DENIED) {
+          console.log('User denied the request for Geolocation.')
+          setError(t('location_declined'))
+        } else {
+          onClose()
+        }
+      })
     } else {
       console.log('Geolocation not supported')
     }
@@ -54,6 +63,11 @@ const MapLocationDialog = ({ open: openDialog, onClose, onLocationApproved }: Pr
         <Button variant="contained" sx={{ mt: 4 }} onClick={handleLocationSelected}>
           {t('allow_location')}
         </Button>
+        {!!error && (
+          <Typography fontSize={15} variant="body1" color={'error.main'}>
+            {error}
+          </Typography>
+        )}
         <Button variant="text" onClick={onClose}>
           {t('not_this_time')}
         </Button>
