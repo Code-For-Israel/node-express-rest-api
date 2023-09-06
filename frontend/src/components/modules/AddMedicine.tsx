93e182463d713806148fbbfd06178f36aa28f28e
Botruzo <65567272+Botruzo@users.noreply.github.com>
2023-09-06 18:55:17 +0300

    Frontend/bug fixes for demo (#33)
    * Fixing desgin bugs
    
    * updates for demo

--- frontend/src/components/modules/AddMedicine.tsx
+++ frontend/src/components/modules/AddMedicine.tsx
@@ -1,6 +1,6 @@
 import FormRadio from '@/components/elements/FormRadio'
 import useStaticTranslation from '@/hooks/useStaticTranslation'
-import { Box, RadioGroup, Stack, Typography } from '@mui/material'
+import { RadioGroup, Stack, Typography } from '@mui/material'
 import { MedicineItemType } from 'MedicineTypes'
 import { ChangeEvent } from 'react'
 
@@ -11,24 +11,20 @@ const AddMedicine = ({ medicine, onSave }: Props) => {
 
   const handlePick = (e: ChangeEvent<HTMLInputElement>, value: string) => {
     onSave(medicine, value)
-    console.log(e)
   }
 
   return (
     <Stack direction={'column'} gap={7} py={3} px={4} sx={{ width: '100%', overflowY: 'auto' }}>
       <Stack gap={2} pt={3} justifyContent={'center'} alignItems={'center'} textAlign={'center'}>
         <Typography variant="h3">{t('will_expire_soon')}</Typography>
-        <Box sx={{ width: '100%', display: 'grid', gridTemplateColumns: '1fr 1em 1fr' }}>
-          <Typography variant="body1" textAlign={'right'}>
-            {medicine.Name}
+        <Stack>
+          <Typography variant="body1" textAlign={'center'} textTransform={'capitalize'}>
+            {medicine.englishName.toLowerCase()}
           </Typography>
           <Typography variant="body1" textAlign={'center'}>
-            |
+            {medicine.Name}
           </Typography>
-          <Typography variant="body1" textAlign={'left'}>
-            {medicine.englishName}
-          </Typography>
-        </Box>
+        </Stack>
       </Stack>
       <RadioGroup sx={{ width: 'fit-content', pl: 4, rowGap: 1 }} aria-label="expired-state" onChange={handlePick}>
         <FormRadio value="inAMonth" label="כן" />
